00ef9ee1bf80ad7383eebc6edbb74135baae384f
aslerch <88261471+aslerch@users.noreply.github.com>
2021-10-22 12:11:47 -0500

    commit 1

--- src/ilstu/edu/Game.java
+++ src/ilstu/edu/Game.java
@@ -1,7 +1,46 @@
 package ilstu.edu;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/** A class that represents a card game */
 public class Game {
 
+    /** Fields */
+    private LinkedList<Card> unshuffledDeck = createDeck();
+    private Stack<Card> drawPile;
+    private Stack<Card> discardPile;
+    private Queue<Card> playerPool;
 
+    /** Constructors */
+    public Game() {}
+    public Game(LinkedList<String> playerPool) {
+        shuffle();
+    }
+
+    /** Methods */
+    /**
+     * Shuffles the deck of cards
+     */
+    private static void shuffle() {
+
+    }
+
+    /**
+     * Creates the deck of cards
+     * @return The deck of cards
+     */
+    private static LinkedList<Card> createDeck() {
+        LinkedList<Card> deck = new LinkedList<Card>();
+        for (int i = 1; i <= 10; i++) {
+            deck.add(new Card(i, "hearts"));
+            deck.add(new Card(i, "spades"));
+            deck.add(new Card(i, "clubs"));
+            deck.add(new Card(i, "diamonds"));
+        }
+        return deck;
+    }
 
 }
--- src/ilstu/edu/Player.java
+++ src/ilstu/edu/Player.java
@@ -1,4 +1,12 @@
 package ilstu.edu;
 
+import java.util.ArrayList;
+
+/** A class that represents a player in a game */
 public class Player {
+
+    /** Fields */
+    private ArrayList<Card> handOfCards = new ArrayList<>();
+
+
 }
