3e4f2f9cb0afe3de5002c7201ab8091d1c93fd43
aslerch <88261471+aslerch@users.noreply.github.com>
2021-11-03 09:48:43 -0500

    commit 3

--- src/ilstu/edu/Player.java
+++ src/ilstu/edu/Player.java
@@ -38,5 +38,75 @@ public class Player {
         }
     }
 
+    public String chooseCardPile() {
+
+    }
+
+    /**
+     * Checks to see if a new card would allow the player to win the game
+     * @param newCard new card to be considered in comparison to the player's hand
+     * @return true if the player could win by adding the new card to their hand, false otherwise
+     */
+    private boolean doesNewCardWinGame(Card newCard) {
+        // creates a temporary hand of five cards including the new card
+        LinkedList<Card> fiveCards = new LinkedList<>();
+        fiveCards.add(newCard);
+        for (Card card : handOfCards) {
+            fiveCards.add(card);
+        }
+        // calculates the total of the four highest cards
+        int total = calculateTotalOfFourHighestCards(fiveCards);
+        // counts how many of each suit is present in the temporary hand of five cards
+        int numOfHearts = 0;
+        int numOfSpades = 0;
+        int numOfClubs = 0;
+        int numOfDiamonds = 0;
+        for (Card card : fiveCards) {
+            if (card.getSuit().equalsIgnoreCase("hearts"))
+                numOfHearts++;
+            if (card.getSuit().equalsIgnoreCase("spades"))
+                numOfSpades++;
+            if (card.getSuit().equalsIgnoreCase("clubs"))
+                numOfClubs++;
+            if (card.getSuit().equalsIgnoreCase("diamonds"))
+                numOfDiamonds++;
+        }
+        // checks win conditions with the new card accounted for
+        if (total >= 20)
+            return true;
+        if (numOfHearts >= 4 || numOfSpades >= 4 || numOfClubs >= 4 || numOfDiamonds >= 4)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Calculates the summation of all values for five cards
+     * @param fiveCards The five cards whose values should be added
+     * @return The total value of all five cards
+     */
+    private int calculateTotalOfFourHighestCards(LinkedList<Card> fiveCards) {
+        int totalOfFive = 0;
+        for (Card card : fiveCards) {
+            totalOfFive += card.getValue();
+        }
+        int totalOfFourHighestCards = totalOfFive - getLowestValueCard(fiveCards).getValue();
+        return totalOfFourHighestCards;
+    }
+
+    /**
+     * Finds the lowest value card in a list of cards
+     * @param fiveCards The list of cards
+     * @return The Card object which has the lowest value
+     */
+    private Card getLowestValueCard(LinkedList<Card> fiveCards) {
+        Card lowestValueCard = fiveCards.get(0);
+        for (Card card : fiveCards) {
+            if (card.getValue() < lowestValueCard.getValue())
+                lowestValueCard = card;
+        }
+        return lowestValueCard;
+    }
+
 
 }
